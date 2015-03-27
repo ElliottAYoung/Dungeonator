b7284944f9b5911371ce1227643fe6eef11d00e9
Elliott Young <elliott.a.young@gmail.com>
2015-03-27 17:13:50 -0500

    Add general, first release of dungeon names

--- lib/dungeonator.rb
+++ lib/dungeonator.rb
@@ -1,5 +1,17 @@
 require "dungeonator/version"
 
 module Dungeonator
-  # Your code goes here...
+  def self.dungeon_name
+    "The #{Dungeonator.second_word.sample} Of #{Dungeonator.fourth_word.sample}"
+  end
+
+  private
+
+  def self.second_word
+    ["Clearing", "Tomb", "Lair", "Peak", "Gathering", "Forest", "Mountain", "Island", "City", "Ascent", "Secret", "Rise", "Purge", "Fall", "Treasure", "House", "Crypt", "Cavern", "Trials", "Mystery", "Knights", "Kingdom", "Mine", "Path", "Lord"]
+  end
+
+  def self.fourth_word
+    ["The Kobold King", "Pelor", "Kobolds", "The Goblin King", "The Forgotten", "Goblins", "The Kobold Queen", "The Goblin Queen", "The Fire Drake", "Kord", "The Undead", "The Ancients", "Order", "Crows", "The Lich", "Boccob", "Moradin", "The Ring", "Mysterious Origins", "The Mummy", "The Divines", "Eternal Youth"]
+  end
 end
